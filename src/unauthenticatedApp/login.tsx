614e37627b9c8425860c0549206d04bc2ba97a5a
DaiWang <1403693160@qq.com>
2022-03-12 12:06:22 +0800

    feat:新增登录loading

--- src/unauthenticatedApp/login.tsx
+++ src/unauthenticatedApp/login.tsx
@@ -2,6 +2,7 @@ import { Button, Form, Input } from "antd";
 import { useAuth } from "context/authContext";
 import { FormEvent } from "react";
 import { LongButton } from "unauthenticatedApp";
+import { useAsync } from "utils/useAsync";
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const LoginPages = ({
@@ -10,9 +11,9 @@ export const LoginPages = ({
   onError: (error: Error) => void;
 }) => {
   const { user, login } = useAuth();
-
+  const { running, isLoading } = useAsync();
   const handleSubmit = (values: { username: string; password: string }) =>
-    login(values).catch(onError);
+    running(login(values).catch(onError));
   return (
     <Form onFinish={handleSubmit}>
       <Form.Item
@@ -28,7 +29,7 @@ export const LoginPages = ({
         <Input placeholder="密码" type="password" id="password" />
       </Form.Item>
       <Form.Item>
-        <LongButton htmlType="submit" type="primary">
+        <LongButton loading={isLoading} htmlType="submit" type="primary">
           登录
         </LongButton>
       </Form.Item>
